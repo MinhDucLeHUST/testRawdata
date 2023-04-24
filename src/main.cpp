0b4125b936503863613a6eaa68f181636f31d521
MinhDucLeHUST <minhducle2082k@gmail.com>
2023-04-24 21:57:09 +0700

    fix sth at night

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include "IRremote.hpp"
+#include "IRReceive.hpp"
 
 #define PUB_TOPIC "data" 
 #define SUB_TOPIC "test"
 #define LED_SENSE 2           // blink when sense IR coming
 #define LED_START 23          // ON to notify start receive IR signal
-#define LED_SEND 15
+#define LED_SEND 15           // Config LED to send IR signal
+#define LED_RECIEVE 13        // Config LED to receive IR signal
 
-const char* ssid = "CAM_TEST_EN";
-const char* password = "doimatkhauroi";
-// const char* ssid = "P407";
-// const char* password = "17052000";
-const char* mqtt_server = "192.168.88.108";
-// const char* mqtt_server = "192.168.1.2";      
+// const char* ssid = "CAM_TEST_EN";
+// const char* password = "doimatkhauroi";
+const char* ssid = "P407";
+const char* password = "17052000";
+// const char* mqtt_server = "192.168.88.108";
+const char* mqtt_server = "192.168.1.2";      
 int i=0;
 
 void handleData(char str[]);
-void showArray (int size_array);
 void stringToUint16(char s[], int sizeArray);
 int countElement (char s[]);
 uint16_t *data_recieve;
@@ -25,7 +26,7 @@ void connectBroker();
 void callback(char* topic, byte *payload, unsigned int length);
 void connect_to_broker();
 void connectWifi (void);
-void sendRawData (const uint16_t* rawData);
+void receiveData ();
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -34,6 +35,8 @@ PubSubClient client(espClient);
 void setup() {
   Serial.begin(9600);
   IrSender.begin(LED_SEND);
+  IrReceiver.begin(LED_RECIEVE,ENABLE_LED_FEEDBACK);
+  // IrReceiver.enableIRIn();
   // irsend.begin(15);
   pinMode(LED_SENSE, OUTPUT);
   pinMode(LED_START, OUTPUT);
@@ -48,6 +51,7 @@ void loop() {
   if (!client.connected()) {
     connectBroker();
   }
+  receiveData();
 }
 
 void connectBroker() {
@@ -66,11 +70,11 @@ void connectBroker() {
 }
 
 void callback(char* topic, byte *payload, unsigned int length) {
-  char data[350];
-  Serial.println("\n-------new message from broker-----");
-  Serial.print("topic: ");
+  char data[1000];
+  Serial.println("\n-------New message from broker-----");
+  Serial.print("Topic: ");
   Serial.println(topic);
-  Serial.print("message: ");
+  Serial.print("Message: ");
   Serial.write(payload, length);
   Serial.println();
   for(int i=0; i<length;i++){
@@ -78,6 +82,7 @@ void callback(char* topic, byte *payload, unsigned int length) {
   }
   Serial.printf("  Data = %s\n",data);
   handleData(data);
+  
 }
 
 void connectWifi (void){
@@ -112,17 +117,10 @@ void stringToUint16(char s[], int sizeArray) {
   uint16_t num16 = (uint16_t)num;
   data_recieve[i] = num16;
   printf("rawdata[%d] = %u\n", i,data_recieve[i]);
-  // i++;
   if(i >= sizeArray-1)
     i=0;
   else 
     i++;
-}
-
-void showArray (int size_array){
-  for(int i=0;i<size_array;i++){
-      printf("%u\t",data_recieve[i]);
-  }
 }
 
 int countElement (char s[]){
@@ -135,8 +133,18 @@ int countElement (char s[]){
   return count_element+1;
 }
 
-void sendRawData (const uint16_t* rawData)
-{
-  int rawDataSize = sizeof(rawData) / sizeof(uint16_t);
-  IrSender.sendRaw(rawData,31, 38);
+void receiveData (){
+  String temp = "";
+  if (IrReceiver.decode()) {
+    int rawlen = IrReceiver.decodedIRData.rawDataPtr->rawlen-1;
+    uint8_t rawCode[RAW_BUFFER_LENGTH];
+    IrReceiver.compensateAndStoreIRResultInArray(rawCode);
+    for (int i = 0; i < rawlen; i++) {
+      printf("%u ", rawCode[i]);
+    }
+    // Serial.printf("Data = %", temp.substring(0,temp.length()-1));
+    Serial.printf("\nLength = %d\n", rawlen);
+    Serial.printf("Name of protocol: %s", ProtocolNames[IrReceiver.decodedIRData.protocol]);
+    IrReceiver.resume(); // Enable receiving of the next value
+  }
 }
